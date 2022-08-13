eeeba7aaa23da14e01a83edb329d130f31fbc973
vipinnagar8700 <96283620+vipinnagar8700@users.noreply.github.com>
2022-08-13 17:01:51 +0530

    added comments in index js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs");
 
 app.get('/', (req, res) => res.send('Hello World'));
 app.get('/home', function(req, res) {
+    // prepare characters 
 var characters = [
     {
       name: 'Harry',
@@ -23,10 +24,11 @@ var characters = [
       designation: "Student"
     }
      ];
+    //prepare subheading
   var subheading = "my name is vipin";
 
 
-
+// provide data to index js in the form of keys and values 
 res.render('index',{characters: characters,
 subheading: subheading});
 
